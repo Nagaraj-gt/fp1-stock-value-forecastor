9438f7fd0e7272a8f785ce56145b2b3c6a7ea829
Nagaraj-gt <109936352+Nagaraj-gt@users.noreply.github.com>
2022-08-06 21:31:44 +0530

    Update setup.py for  path identification

--- code/setup.py
+++ code/setup.py
@@ -35,7 +35,7 @@ if ON_COLAB:
         if os.system(cmd) != 0:
             print('  --> ERROR')
 
-    print("\nAdditional setup ...")
+    print("\nAdditional setup for torch, nltk and requirements")
     setup_cmds = [
         'pip install torch==1.7.0+cu101 torchvision==0.8.1+cu101 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html',
 	'pip install -r code/requirements.txt',
